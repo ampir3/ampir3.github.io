54761d87f69bde6ce9fb8d2da93d7109ab81f95d
anotherSimpleCoder <71701743+anotherSimpleCoder@users.noreply.github.com>
2021-02-28 23:06:58 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+<title>Ampir3.com</title>
+<meta charset="UTF-8">
+<link rel="stylesheet" href="assets/css/Main.css">
+</head>
+<body>
+  <header>
+    <div class="titlescreen">
+        <div class="headerTitleBox">
+            <div class="headerTitle"><img src="assets/pictures/Logo.png" alt=""></div>
+            <div class="socialmedia">
+                
+            </div>
+        </div>
+    </div>
+  </header>
+  <main>
+    <section class="Welcome">
+      <h1>Welcome</h1>
+    </section>
+    <section class="About Me">
+      <h1>About Me</h1>
+    </section>
+    <section class="Releases">
+      <h1>Releases</h1>
+    </section>
+    <section class="Contact">
+      <h1>Contact</h1>
+    </section>
+  </main>
+  <footer class="footer">
+      © <a href="https://KFPLMusic.com/">KFPL</a> and <a href="https://Ampir3.com/">Ampir3</a> <?php echo '<span>'.date("Y").'</span>'?><br>
+  </footer>
+</body>
+</html>
